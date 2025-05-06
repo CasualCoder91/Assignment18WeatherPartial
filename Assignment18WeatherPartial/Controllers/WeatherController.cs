996e273a5f495f1f0d34c87357a5bd8851bbf882
Alarich Herzner <alarich2@hotmail.com>
2025-05-06 16:20:21 +0200

    Add project files.

---
+++ Assignment18WeatherPartial/Controllers/WeatherController.cs
@@ -0,0 +1,34 @@
+﻿using Assignment18WeatherPartial.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment18WeatherPartial.Controllers
+{
+    public class WeatherController : Controller
+    {
+        private List<CityWeather> entries = new()
+        {
+            new() { CityUniqueCode = "LDN", CityName = "London", DateAndTime = DateTime.Parse("2030-01-01 8:00"), TemperatureFahrenheit = 33},
+            new() { CityUniqueCode = "NYC", CityName = "New York", DateAndTime = DateTime.Parse("2030-01-01 3:00"), TemperatureFahrenheit = 60},
+            new() { CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = DateTime.Parse("2030-01-01 9:00"), TemperatureFahrenheit = 82},
+        };
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View(entries); //Views/Weather/Index.cshtml
+        }
+
+        [Route("/weather/{cityCode}")]
+        public IActionResult Details(string? cityCode)
+        {
+            CityWeather? entry = entries.Where(e => e.CityUniqueCode == cityCode).FirstOrDefault();
+
+            if (entry == null)
+            {
+                return BadRequest("No city found for the provided code");
+            }
+
+            return View(entry); //Views/Weather/Details.cshtml
+        }
+    }
+}
---
+++ Assignment18WeatherPartial/Models/CityWeather.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment18WeatherPartial.Models
+{
+    public class CityWeather
+    {
+        public required string CityUniqueCode { get; set; }
+        public required string CityName { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public int TemperatureFahrenheit { get; set; }
+    }
+}
